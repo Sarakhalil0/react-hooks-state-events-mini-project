78663adfe3c1d1e1772968a219c2d0e59f2e013b
sara khalil <sarakhalil3377@gmail.com>
2022-10-12 23:16:56 +0300

    DONE

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -6,14 +6,28 @@ import TaskList from "./TaskList";
 import { CATEGORIES, TASKS } from "../data";
 console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
-
+//
 function App() {
+  const [tasks, setTasks] = useState(TASKS)
+  const [categories, setCategories] = useState(CATEGORIES)
+//
+
+function filterByCategories(e) {
+  const filteredTasks = TASKS.filter((task) => {
+   if(e.target.value === 'All') 
+   return true
+   else return task.category === e.target.value
+    })
+   setTasks(filteredTasks)
+ }
+
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
+      <CategoryFilter categories={categories} categoriesFilter={filterByCategories} />
       <NewTaskForm />
-      <TaskList />
+      <TaskList tasks={tasks}  />
     </div>
   );
 }
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories, categoriesFilter}) {
+  //
+const [buttonClicked, setButtonClicked] = useState('')
+
+//
+function handleClick(e){
+  categoriesFilter(e)
+  setButtonClicked(e.target.value)
+}
+//
+const className = buttonClicked === Category ? "selected" : "";
+
+//
+ const displayButton = categories.map((Category ,index) => {
+ return  <button className={className} key={index} value={Category}onClick={handleClick}>{Category}</button> }) 
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {displayButton}
     </div>
   );
 }
